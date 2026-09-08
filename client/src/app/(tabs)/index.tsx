620f4b4323cbb4c99c6f736cb01f3a6a57ced63b
Fred Munyao <fredrickmumu2@gmail.com>
2026-09-08 17:12:11 +0300

    Added context for wishlist and cart feature.

--- client/src/app/(tabs)/index.tsx
+++ client/src/app/(tabs)/index.tsx
@@ -64,6 +64,8 @@ fetchProducts()
                 resizeMode='cover'
               />
 
+                <View className='absolute inset-0 bg-black/40' />
+
                 <View className='absolute bottom-4 left-4 z-10'>
                   <Text className='text-white text-2xl font-bold'>{banner.title}</Text>
                    <Text className='text-white text-sm font-medium'>{banner.subtitle}</Text>
@@ -74,7 +76,7 @@ fetchProducts()
                    
                 </View>
 
-                <View className='absolute inset-0 bg-black/40' />
+                
 
             </View>
           ))}
