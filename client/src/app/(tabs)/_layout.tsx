cf7654498fd2ab0cc2cac76312ee59550a1bb56f
Fred Munyao <fredrickmumu2@gmail.com>
2026-09-10 16:06:34 +0300

    Cart UI completed.

--- client/src/app/(tabs)/_layout.tsx
+++ client/src/app/(tabs)/_layout.tsx
@@ -2,8 +2,13 @@ import { Tabs } from 'expo-router'
 import { Ionicons, Feather } from '@expo/vector-icons'
 import React from 'react'
 import { COLORS } from '@/constants'
+import { useCart } from '../../../context/CartContext'
+import { View } from 'react-native'
 
 export default function TabLayout() {
+
+const {cartItems} = useCart()
+
   return (
     <Tabs
       screenOptions={{
@@ -33,7 +38,17 @@ export default function TabLayout() {
         name="cart"
         options={{
           tabBarIcon: ({ color, focused }) => (
-            <Feather name="shopping-cart" size={26} color={color} />
+           <View className='relative'>
+             <Feather name={focused ? "shopping-cart" : "shopping-cart"} size={26} color={color} />
+
+             {cartItems?.length > 0 && 
+
+             <View className='absolute -top-2 -right-2 bg-accent size-3 rounded-full
+             items-center justify-center'>
+              <Ionicons name='ellipse' size={6} color="white" />
+             </View>
+        }
+           </View>
           ),
         }}
       />
